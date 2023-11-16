63c431b3e2eb9b7667c752fbbcc3d6fecd2f4581
szt2csonik <szt2csonik@vasvari.org>
2023-11-16 11:31:54 +0100

    Initial commit

---
+++ src/Kert.java
@@ -0,0 +1,9 @@
+public class Kert {
+    static void kertAllapot(){
+
+        Noveny.ViragNev=new Noveny();
+    }
+    static void ontoz(){
+
+    }
+}
---
+++ src/Noveny.java
@@ -0,0 +1,8 @@
+public class Noveny {
+    String ViragNev;
+    String FaNev;
+    public Noveny(String viragNev,String faNev){
+        ViragNev=viragNev;
+        FaNev=faNev;
+    }
+}
